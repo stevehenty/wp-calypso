eb7ff4436bccc8e4e0eb604ca9b8b609a9880ef5
Bill Robbins <billrobbins@users.noreply.github.com>
2024-03-07 12:50:25 -0600

    Initial commit

--- client/lib/checkout/payment-methods.tsx
+++ client/lib/checkout/payment-methods.tsx
@@ -8,18 +8,21 @@ import creditCardPlaceholderImage from 'calypso/assets/images/upgrades/cc-placeh
 import creditCardUnionPayImage from 'calypso/assets/images/upgrades/cc-unionpay.svg';
 import creditCardVisaImage from 'calypso/assets/images/upgrades/cc-visa.svg';
 import payPalImage from 'calypso/assets/images/upgrades/paypal.svg';
+import razorpayImage from 'calypso/assets/images/upgrades/razorpay.svg';
 
 export const PARTNER_PAYPAL_EXPRESS = 'paypal_express';
-export const PAYMENT_AGREEMENTS_PARTNERS = [ PARTNER_PAYPAL_EXPRESS ];
+export const PARTNER_RAZORPAY = 'razorpay';
+export const PAYMENT_AGREEMENTS_PARTNERS = [ PARTNER_PAYPAL_EXPRESS, PARTNER_RAZORPAY ];
 
 /**
- * A saved payment method (card or PayPal agreement).
+ * A saved payment method (card, PayPal agreement, or Razorpay eMandate).
  *
  * Used by the `/me/payment-methods` endpoint after version 1.1.
  */
 export type StoredPaymentMethod =
 	| StoredPaymentMethodBase
 	| StoredPaymentMethodPayPal
+	| StoredPaymentMethodRazorpay
 	| StoredPaymentMethodCard
 	| StoredPaymentMethodEbanx
 	| StoredPaymentMethodStripeSource;
@@ -49,6 +52,10 @@ export interface StoredPaymentMethodBase {
 
 export interface StoredPaymentMethodPayPal extends StoredPaymentMethodBase {
 	payment_partner: 'paypal_express';
+}
+
+export interface StoredPaymentMethodRazorpay extends StoredPaymentMethodBase {
+	payment_partner: 'razorpay';
 }
 
 export interface StoredPaymentMethodCard extends StoredPaymentMethodBase {
@@ -108,6 +115,7 @@ const CREDIT_CARD_SELECTED_PATHS: ImagePathsMap = {
 	visa: creditCardVisaImage,
 	paypal_express: payPalImage,
 	paypal: payPalImage,
+	razorpay: razorpayImage,
 };
 
 const CREDIT_CARD_DEFAULT_PATH = creditCardPlaceholderImage;
@@ -130,6 +138,9 @@ export const PaymentMethodSummary = ( {
 	const translate = useTranslate();
 	if ( type === PARTNER_PAYPAL_EXPRESS ) {
 		return <>{ email || '' }</>;
+	}
+	if ( type === PARTNER_RAZORPAY ) {
+		return <>{ email || translate( 'Razorpay' ) }</>;
 	}
 	let displayType: TranslateResult;
 	switch ( type && type.toLocaleLowerCase() ) {
