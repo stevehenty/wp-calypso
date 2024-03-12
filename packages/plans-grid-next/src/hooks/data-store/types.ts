e56e5602154e29e336a6635098327381c38435fe
oswian <oswian123@gmail.com>
2024-03-12 15:40:59 +1100

    Re-add dopped param comments

--- packages/plans-grid-next/src/hooks/data-store/types.ts
+++ packages/plans-grid-next/src/hooks/data-store/types.ts
@@ -4,20 +4,20 @@ import { HiddenPlans, PlansIntent } from '../../types';
 import { UseFreeTrialPlanSlugs } from './use-grid-plans';
 
 export interface UseGridPlansParams {
-	allFeaturesList: FeatureList;
+	allFeaturesList: FeatureList; // Temporary until feature definitions are ported to calypso-products package
 	coupon?: string;
 	eligibleForFreeHostingTrial?: boolean;
 	hiddenPlans?: HiddenPlans;
 	intent?: PlansIntent;
 	isDisplayingPlansNeededForFeature?: boolean;
 	isInSignup?: boolean;
-	isSubdomainNotGenerated?: boolean;
+	isSubdomainNotGenerated?: boolean; // If the subdomain generation is unsuccessful we do not show the free plan
 	selectedFeature?: string | null;
 	selectedPlan?: PlanSlug;
 	showLegacyStorageFeature?: boolean;
 	siteId?: number | null;
 	storageAddOns: ( AddOnMeta | null )[];
-	term?: ( typeof TERMS_LIST )[ number ];
+	term?: ( typeof TERMS_LIST )[ number ]; // defaults to monthly
 	useCheckPlanAvailabilityForPurchase: Plans.UseCheckPlanAvailabilityForPurchase;
 	useFreeTrialPlanSlugs?: UseFreeTrialPlanSlugs;
 }
